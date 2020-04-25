aba5074960c4daaa7c4eecd2ebb133e22bd2bd4c
Idhrendur <idhrendur@hotmail.com>
2020-04-25 15:02:25 -0700

    Clean up CardinalToOrdinal

--- CardinalToOrdinal.cpp
+++ CardinalToOrdinal.cpp
@@ -1,48 +1,25 @@
-/*Copyright (c) 2018 The Paradox Game Converters Project
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
-
-
 #include "CardinalToOrdinal.h"
 
 
 
-std::string CardinalToOrdinal(int cardinal)
+std::string CardinalToOrdinal(const int cardinal)
 {
-	const int hundredRem = cardinal % 100;
-	const int tenRem = cardinal % 10;
-	if (hundredRem - tenRem == 10)
+	const auto hundredRemainder = cardinal % 100;
+	const auto tenRemainder = cardinal % 10;
+	if (hundredRemainder - tenRemainder == 10)
 	{
 		return "th";
 	}
 
-	switch (tenRem)
+	switch (tenRemainder)
 	{
-	case 1:
-		return "st";
-	case 2:
-		return "nd";
-	case 3:
-		return "rd";
-	default:
-		return "th";
+		case 1:
+			return "st";
+		case 2:
+			return "nd";
+		case 3:
+			return "rd";
+		default:
+			return "th";
 	}
 }
