97930842a7c77c6195f64467ef621a184a95c59f
Idhrendur <idhrendur@hotmail.com>
2020-04-25 14:38:32 -0700

    Take the hatchet to the old parser

--- Color.cpp
+++ Color.cpp
@@ -2,95 +2,74 @@
 #include <chrono>
 #include <random>
 #include <sstream>
-#include "Object.h"
 
-commonItems::Color::Color():
-	initialized(false),
-	c({ 0, 0, 0 })
-{}
+commonItems::Color::Color() : initialized(false), c({0, 0, 0}) {}
 
-
-commonItems::Color::Color(const int r, const int g, const int b):
-	initialized(true),
-	c({ r, g, b })
-{}
-
-
-commonItems::Color::Color(std::shared_ptr<Object> colorObject):
-	initialized(false),
-	c({ 0, 0, 0 })
+commonItems::Color::Color(const int r, const int g, const int b)
+    : initialized(true), c({r, g, b})
 {
-	std::stringstream colorStream;
-	colorStream << *colorObject;
-	auto unneeded = getNextToken(colorStream);
-	Color newColor(colorStream);
-	newColor.GetRGB(c[0], c[1], c[2]);
 }
 
-
-commonItems::Color::Color(std::istream& theStream):
-	initialized(false),
-	c({ 0, 0, 0 })
+commonItems::Color::Color(std::istream& theStream)
+    : initialized(false), c({0, 0, 0})
 {
-	unsigned int colorsInitialized = 0;
+  unsigned int colorsInitialized = 0;
 
-	auto token = getNextToken(theStream);
-	while (token && (*token != "}"))
-	{
-		if ((token->find('=') == std::string::npos) && (token->find('{') == std::string::npos))
-		{
-			if (token->substr(0,1) == "\"")
-			{
-				token = token->substr(1, token->length() - 2);
-			}
-			c[colorsInitialized] = std::stoi(*token);
-			colorsInitialized++;
-		}
-		token = getNextToken(theStream);
-	}
+  auto token = getNextToken(theStream);
+  while (token && (*token != "}"))
+  {
+    if ((token->find('=') == std::string::npos) &&
+        (token->find('{') == std::string::npos))
+    {
+      if (token->substr(0, 1) == "\"")
+      {
+	token = token->substr(1, token->length() - 2);
+      }
+      c[colorsInitialized] = std::stoi(*token);
+      colorsInitialized++;
+    }
+    token = getNextToken(theStream);
+  }
 
-	initialized = (colorsInitialized > 2);
+  initialized = (colorsInitialized > 2);
 }
-
 
 void commonItems::Color::RandomlyFlunctuate(const int stdDev)
 {
-	// All three color components will go up or down by the some amount (according to stdDev), 
-	// and then each is tweaked a bit more (with a much smaller standard deviation).
-	static std::mt19937 generator(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
-	const double allChange = std::normal_distribution<double>(0.0, stdDev)(generator);	// the amount the colors all change by
-	std::normal_distribution<double> distribution(0.0, stdDev / 4.0);
-	for (auto& component : c)	// the component under consideration
-	{
-		component += lround(allChange + distribution(generator));
-		if (component < 0)
-		{
-			component = 0;
-		}
-		else if (component > 255)
-		{
-			component = 255;
-		}
-	}
+  // All three color components will go up or down by the some amount (according
+  // to stdDev), and then each is tweaked a bit more (with a much smaller
+  // standard deviation).
+  static std::mt19937 generator(static_cast<unsigned int>(
+      std::chrono::system_clock::now().time_since_epoch().count()));
+  const double allChange = std::normal_distribution<double>(0.0, stdDev)(
+      generator); // the amount the colors all change by
+  std::normal_distribution<double> distribution(0.0, stdDev / 4.0);
+  for (auto& component : c) // the component under consideration
+  {
+    component += lround(allChange + distribution(generator));
+    if (component < 0)
+    {
+      component = 0;
+    }
+    else if (component > 255)
+    {
+      component = 255;
+    }
+  }
 }
 
-
-std::ostream& commonItems::operator<<(std::ostream& out, const commonItems::Color& color)
+std::ostream& commonItems::operator<<(std::ostream& out,
+                                      const commonItems::Color& color)
 {
-	out << color.c[0] << ' ' << color.c[1] << ' ' << color.c[2];
-	return out;
+  out << color.c[0] << ' ' << color.c[1] << ' ' << color.c[2];
+  return out;
 }
-
 
 void commonItems::Color::GetRGB(int& r, int& g, int& b) const
 {
-	r = c[0];
-	g = c[1];
-	b = c[2];
+  r = c[0];
+  g = c[1];
+  b = c[2];
 }
 
-
-commonItems::Color::operator bool() const
-{
-	return initialized;
-}
+commonItems::Color::operator bool() const { return initialized; }
--- Color.h
+++ Color.h
@@ -1,41 +1,37 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "newParser.h"
 #include <array>
 #include <iostream>
 #include <memory>
-#include "newParser.h"
-
-class Object;
 
 namespace commonItems
 {
-	class Color: parser
-	{
-		public:
-			Color();
-			Color(int r, int g, int b);
-			explicit Color(std::shared_ptr<Object> colorObject);
-			explicit Color(std::istream& theStream);
+class Color : parser
+{
+public:
+  Color();
+  Color(int r, int g, int b);
+  explicit Color(std::istream& theStream);
 
-			void RandomlyFlunctuate(int stdDev);
+  void RandomlyFlunctuate(int stdDev);
 
-			friend std::ostream& operator<<(std::ostream&, const Color&);
+  friend std::ostream& operator<<(std::ostream&, const Color&);
 
-			void GetRGB(int& r, int& g, int& b) const;
-			[[nodiscard]] int r() const { return c[0]; }
-			[[nodiscard]] int g() const { return c[1]; }
-			[[nodiscard]] int b() const { return c[2]; }
+  void GetRGB(int& r, int& g, int& b) const;
+  [[nodiscard]] int r() const { return c[0]; }
+  [[nodiscard]] int g() const { return c[1]; }
+  [[nodiscard]] int b() const { return c[2]; }
 
-			operator bool() const; // Returns true if the color has been initialized with an RGB triplet.
+  operator bool() const; // Returns true if the color has been initialized with
+                         // an RGB triplet.
 
-		private:
-			bool initialized;
-			std::array<int, 3> c;
-	};
-	std::ostream& operator<<(std::ostream&, const Color&);
-}
-
-
+private:
+  bool initialized;
+  std::array<int, 3> c;
+};
+std::ostream& operator<<(std::ostream&, const Color&);
+} // namespace commonItems
 
 #endif
