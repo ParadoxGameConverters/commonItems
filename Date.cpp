2990a6f32dd8934ff64665b5f4a1a8c23755eccc
Idhrendur <idhrendur@hotmail.com>
2020-04-25 17:06:16 -0700

    Clean up date

--- Date.cpp
+++ Date.cpp
@@ -1,105 +1,115 @@
-/*Copyright (c) 2018 The Paradox Game Converters Project
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
-
-
 #include "Date.h"
-#include <array>
-#include <vector>
 #include "OSCompatibilityLayer.h"
+#include <array>
 
 
 
-date::date(std::string _init):
-	year(1),
-	month(1),
-	day(1)
+date::date(std::string init)
 {
-	if (_init.length() < 1)
+	if (init.length() < 1)
 	{
 		return;
 	}
 
-	if (_init[0] == '\"')
+	if (init[0] == '\"')
 	{
-		_init = _init.substr(1, _init.length() - 2);
+		init = init.substr(1, init.length() - 2);
 	}
-	const int first_dot	= _init.find_first_of('.');	// the position of the first period in the date
-	const int last_dot	= _init.find_last_of('.');		// the position of the second period in the date
+	const auto first_dot = init.find_first_of('.');
+	const auto last_dot = init.find_last_of('.');
 
 	try
 	{
-		year						= stoi( _init.substr(0, first_dot) );
-		month						= stoi( _init.substr(first_dot + 1, last_dot - first_dot) );
-		day						= stoi( _init.substr(last_dot + 1, 2) );
+		year = stoi(init.substr(0, first_dot));
+		month = stoi(init.substr(first_dot + 1, last_dot - first_dot));
+		day = stoi(init.substr(last_dot + 1, 2));
 	}
 	catch (const std::exception& e)
 	{
 		LOG(LogLevel::Warning) << "Problem inputting date: " << e.what();
-		year = 0;
-		month = 0;
-		day = 0;
+		year = 1;
+		month = 1;
+		day = 1;
 	}
 }
 
 
-bool date::operator==(const date& _rhs) const noexcept
+void date::increaseByMonths(const int months)
 {
-	return ((year == _rhs.year)
-		 && (month == _rhs.month)
-		 && (day == _rhs.day));
+	year += months / 12;
+	month += months % 12;
+	if (month > 12)
+	{
+		year++;
+		month -= 12;
+	}
 }
 
 
-bool date::operator!=(const date& _rhs) const noexcept
+void date::subtractYears(const int years)
 {
-	return !(*this == _rhs);
+	year -= years;
 }
 
 
-bool date::operator<(const date& _rhs) const noexcept
+bool date::operator==(const date& rhs) const
 {
-	return ((year < _rhs.year)
-		|| ((year == _rhs.year) && (month < _rhs.month))
-		|| ((year == _rhs.year) && (month == _rhs.month) && (day < _rhs.day)));
+	return ((year == rhs.year) && (month == rhs.month) && (day == rhs.day));
 }
 
 
-bool date::operator>(const date& _rhs) const noexcept
+bool date::operator!=(const date& rhs) const
 {
-	return ((year > _rhs.year)
-		|| ((year == _rhs.year) && (month > _rhs.month))
-		|| ((year == _rhs.year) && (month == _rhs.month) && (day > _rhs.day)));
+	return !(*this == rhs);
 }
 
 
-bool date::operator<=(const date& _rhs) const noexcept
+bool date::operator<(const date& rhs) const
 {
-	return ((*this == _rhs) || (*this < _rhs));
+	return ((year < rhs.year) || ((year == rhs.year) && (month < rhs.month)) ||
+			  ((year == rhs.year) && (month == rhs.month) && (day < rhs.day)));
 }
 
 
-bool date::operator>=(const date& _rhs) const noexcept
-{ 
-	return ((*this == _rhs) || (*this > _rhs));
+bool date::operator>(const date& rhs) const
+{
+	return ((year > rhs.year) || ((year == rhs.year) && (month > rhs.month)) ||
+			  ((year == rhs.year) && (month == rhs.month) && (day > rhs.day)));
+}
+
+
+bool date::operator<=(const date& rhs) const
+{
+	return ((*this == rhs) || (*this < rhs));
+}
+
+
+bool date::operator>=(const date& rhs) const
+{
+	return ((*this == rhs) || (*this > rhs));
+}
+
+
+float date::diffInYears(const date& rhs) const
+{
+	auto years = static_cast<float>(year - rhs.year);
+	years += static_cast<float>(calculateDayInYear() - rhs.calculateDayInYear()) / 365;
+
+	return years;
+}
+
+
+bool date::isSet() const
+{
+	return *this != date{};
+}
+
+
+std::string date::toString() const
+{
+	std::stringstream buf;
+	buf << year << '.' << month << '.' << day;
+	return buf.str();
 }
 
 
@@ -110,51 +120,21 @@ std::ostream& operator<<(std::ostream& out, const date& d)
 }
 
 
-float date::diffInYears(const date& _rhs) const noexcept
-{
-	float years = static_cast<float>(year - _rhs.year);	// the difference in years
-	years += static_cast<float>(calculateDayInYear() - _rhs.calculateDayInYear()) / 365;
-
-	return years;
-}
-
-
-const std::array<int, 12> daysByMonth = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
-int date::calculateDayInYear() const noexcept
+const std::array<int, 12> daysByMonth{
+	 0,	// January
+	 31,	// February
+	 59,	// March
+	 90,	// April
+	 120, // May
+	 151, // June
+	 181, // July
+	 212, // August
+	 243, // September
+	 273, // October
+	 304, // November
+	 334	// December
+};
+int date::calculateDayInYear() const
 {
 	return day + daysByMonth[month - 1];
-}
-
-
-void date::increaseByMonths(const int _months) noexcept
-{
-	year += _months / 12;
-	month += _months % 12;
-	if (month > 12)
-	{
-		year++;
-		month -= 12;
-	}
-	return;
-}
-
-
-void date::subtractYears(const int _years) noexcept
-{
-	year -= _years;
-}
-
-
-bool date::isSet() const noexcept
-{
-	const date default_date;	// an instance with the default date
-	return (*this != default_date);
-}
-
-
-std::string date::toString() const
-{
-	std::stringstream buf;
-	buf << year << '.' << month << '.' << day;
-	return buf.str();
 }
