0f89786c9c4ae065bed6415e955bbe0a937463d7
Idhrendur <idhrendur@hotmail.com>
2020-08-22 19:02:49 -0700

    newColor (#61)
    * Branch color to newColor
    * No longer store initialized. Optionals should be used instead.
    * Remove deprecated function.
    * Simplify access to the components.
    * Throw exceptions on bad input.
    * Remove self-parsing constructor.
    * Constructor takes an array
    * Adjust output
    * Handle different color spaces
    * Add equality operators
    * Update newColor.cpp
    
    Co-authored-by: IhateTrains <IhateTrains@users.noreply.github.com>

---
+++ newColor.h
@@ -0,0 +1,57 @@
+#ifndef NEW_COLOR_H
+#define NEW_COLOR_H
+
+
+
+#include "Parser.h"
+#include <array>
+#include <iostream>
+
+
+
+namespace commonItems
+{
+
+class newColor
+{
+  public:
+	class Factory;
+	enum class ColorSpaces
+	{
+		UNSPECIFIED, RGB, HSV
+	};
+	newColor() = default;
+	newColor(const std::array<int, 3> components, ColorSpaces colorSpace = ColorSpaces::UNSPECIFIED): components(components), colorSpace(colorSpace) {}
+
+	bool operator==(const newColor& rhs) const;
+	bool operator!=(const newColor& rhs) const;
+
+	[[nodiscard]] const auto& getComponents() const { return components; }
+	[[nodiscard]] const auto& getColorSpace() const { return colorSpace; }
+
+	// All three color components will go up or down by the some amount (according to stdDev), and then each is tweaked a
+	// bit more (with a much smaller standard deviation).
+	void RandomlyFluctuate(int stdDev);
+
+	friend std::ostream& operator<<(std::ostream&, const newColor&);
+
+  private:
+	std::array<int, 3> components{0, 0, 0};
+	ColorSpaces colorSpace = ColorSpaces::UNSPECIFIED;
+};
+
+
+std::ostream& operator<<(std::ostream&, const newColor&);
+
+
+class newColor::Factory: parser
+{
+  public:
+	static newColor getColor(std::istream& theStream);
+};
+
+} // namespace commonItems
+
+
+
+#endif // NEW_COLOR_H
