10bca3694cdc22a2c7756feb61716a88c1dfd579
IhateTrains <IhateTrains@users.noreply.github.com>
2020-10-26 21:06:19 +0100

    Replaced usage of `localtime` function which causes warnings (#108)
    * localtime replacement
    
    * Added missing delete
    
    * Removed unneeded dynamic allocation

--- Log.cpp
+++ Log.cpp
@@ -6,10 +6,6 @@
 #include <map>
 
 
-#ifdef _WIN32
-#pragma warning(disable : 4996) // suppress warning about localtime, as the alternative is windows-specific
-#endif
-
 
 Log::Log(const LogLevel level): logLevel(level)
 {
@@ -52,10 +48,17 @@ void Log::WriteTheTime(std::ostream& logFile)
 {
 	time_t rawTime;
 	time(&rawTime);
-	const tm* timeInfo = localtime(&rawTime);
+
+	tm timeInfo{};
+	#if (defined(WIN32) || defined(_WIN32) || defined(__WIN32__))
+	localtime_s(&timeInfo, &rawTime);
+	#else
+	localtime_r(&rawTime, &timeInfo); // POSIX
+	#endif
+	
 
 	char timeBuffer[64];
-	const auto bytesWritten = strftime(timeBuffer, sizeof timeBuffer, "%Y-%m-%d %H:%M:%S ", timeInfo);
+	const auto bytesWritten = strftime(timeBuffer, sizeof timeBuffer, "%Y-%m-%d %H:%M:%S ", &timeInfo);
 	if (bytesWritten != 0)
 	{
 		logFile << timeBuffer;
