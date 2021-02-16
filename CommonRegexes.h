3d87dceb3d8ae05054ed1910869d7fe4575adc96
IhateTrains <IhateTrains@users.noreply.github.com>
2021-02-16 01:30:02 +0100

    Removed unneeded escaping backslashes in regexes (#134)

--- CommonRegexes.h
+++ CommonRegexes.h
@@ -8,17 +8,17 @@ namespace commonItems
 // catchall:
 //		We grab everything that's NOT =, { or }, OR we grab everything within quotes, except newlines, which we already drop
 //		in the parser.
-static inline const char* catchallRegex = R"([^=^{^}]+|\".+\")";
+static inline const char* catchallRegex = R"([^=^{^}]+|".+")";
 
 // numbers
 static inline const char* integerRegex = R"(-?\d+)";
-static inline const char* quotedIntegerRegex = R"(\"-?\d+\")";
+static inline const char* quotedIntegerRegex = R"("-?\d+")";
 static inline const char* floatRegex = R"(-?\d+(.\d+)?)";
-static inline const char* quotedFloatRegex = R"(\"-?\d+(.\d+)?\")";
+static inline const char* quotedFloatRegex = R"("-?\d+(.\d+)?")";
 
 // strings
 static inline const char* stringRegex = R"([^[:s:]^=^\{^\}^\"]+)";
-static inline const char* quotedStringRegex = R"(\"[^\n^=^\{^\}^\"]+\")";
+static inline const char* quotedStringRegex = R"("[^\n^=^\{^\}^\"]+")";
 
 
 // compile time regexes, cool stuff
