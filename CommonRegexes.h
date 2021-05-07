e905578bc6c17f79e55f0acea45f693ababd9459
IhateTrains <IhateTrains@users.noreply.github.com>
2021-05-07 03:57:18 +0200

    Fix unused variable warnings in GCC builds (#146)
    * CommonRegexes.cpp
    
    * Update CommonRegexes.cpp
    
    * Alternative solution
    
    * Update CMakeLists.txt
    
    * Update CommonRegexes.h

--- CommonRegexes.h
+++ CommonRegexes.h
@@ -8,18 +8,20 @@ namespace commonItems
 // catchall:
 //		We grab everything that's NOT =, { or }, OR we grab everything within quotes, except newlines, which we already drop
 //		in the parser.
-static inline const char* catchallRegex = R"([^=^{^}]+|".+")";
+inline constexpr const char* catchallRegex = R"([^=^{^}]+|".+")";
 
 // numbers
-static inline const char* integerRegex = R"(-?\d+)";
-static inline const char* quotedIntegerRegex = R"("-?\d+")";
-static inline const char* floatRegex = R"(-?\d+(.\d+)?)";
-static inline const char* quotedFloatRegex = R"("-?\d+(.\d+)?")";
+inline constexpr const char* integerRegex = R"(-?\d+)";
+inline constexpr const char* quotedIntegerRegex = R"("-?\d+")";
+inline constexpr const char* floatRegex = R"(-?\d+(.\d+)?)";
+inline constexpr const char* quotedFloatRegex = R"("-?\d+(.\d+)?")";
 
 // strings
-static inline const char* stringRegex = R"([^[:s:]^=^\{^\}^\"]+)";
-static inline const char* quotedStringRegex = R"("[^\n^=^\{^\}^\"]+")";
+inline constexpr const char* stringRegex = R"([^[:s:]^=^\{^\}^\"]+)";
+inline constexpr const char* quotedStringRegex = R"("[^\n^=^\{^\}^\"]+")";
 
 } // namespace commonItems
+
+
 
 #endif
