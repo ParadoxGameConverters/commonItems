65bbba5e19cb070737cf749d5d14ff803ce5e28e
Ivo Elezović <ivo@element.hr>
2022-02-05 17:46:41 +0100

    utf8toutf16 replacement

--- LinuxUtils.cpp
+++ LinuxUtils.cpp
@@ -593,8 +593,74 @@ std::wstring convertWin1252ToUTF16(const std::string& Win1252)
 */
 std::wstring convertUTF8ToUTF16(const std::string& UTF8)
 {
-	using namespace std;
-	return ConvertString<string, wstring>("UTF-8", "wchar_t//TRANSLIT", UTF8);
+	std::vector<unsigned long> unicode;
+	size_t i = 0;
+	while (i < UTF8.size())
+	{
+		unsigned long uni;
+		size_t todo;
+		bool error = false;
+		unsigned char ch = UTF8[i++];
+		if (ch <= 0x7F)
+		{
+			uni = ch;
+			todo = 0;
+		}
+		else if (ch <= 0xBF)
+		{
+			throw std::logic_error("not a UTF-8 string");
+		}
+		else if (ch <= 0xDF)
+		{
+			uni = ch & 0x1F;
+			todo = 1;
+		}
+		else if (ch <= 0xEF)
+		{
+			uni = ch & 0x0F;
+			todo = 2;
+		}
+		else if (ch <= 0xF7)
+		{
+			uni = ch & 0x07;
+			todo = 3;
+		}
+		else
+		{
+			throw std::logic_error("not a UTF-8 string");
+		}
+		for (size_t j = 0; j < todo; ++j)
+		{
+			if (i == UTF8.size())
+				throw std::logic_error("not a UTF-8 string");
+			unsigned char ch = UTF8[i++];
+			if (ch < 0x80 || ch > 0xBF)
+				throw std::logic_error("not a UTF-8 string");
+			uni <<= 6;
+			uni += ch & 0x3F;
+		}
+		if (uni >= 0xD800 && uni <= 0xDFFF)
+			throw std::logic_error("not a UTF-8 string");
+		if (uni > 0x10FFFF)
+			throw std::logic_error("not a UTF-8 string");
+		unicode.push_back(uni);
+	}
+	std::wstring utf16;
+	for (size_t i = 0; i < unicode.size(); ++i)
+	{
+		unsigned long uni = unicode[i];
+		if (uni <= 0xFFFF)
+		{
+			utf16 += (wchar_t)uni;
+		}
+		else
+		{
+			uni -= 0x10000;
+			utf16 += (wchar_t)((uni >> 10) + 0xD800);
+			utf16 += (wchar_t)((uni & 0x3FF) + 0xDC00);
+		}
+	}
+	return utf16;
 }
 
 
