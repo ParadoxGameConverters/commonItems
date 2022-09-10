538d8dcf92286c91eddbef05518352decba8a1cf
Idhrendur <idhrendur@hotmail.com>
2022-09-10 13:42:54 -0700

    Add most LocalizationDatabase tests

--- tests/Localization/LocalizationDatabaseTests.cpp
+++ tests/Localization/LocalizationDatabaseTests.cpp
@@ -6,9 +6,62 @@
 
 TEST(Localization_LocalizationDatabase_Tests, NewLocalizationDatabaseIsEmpty)
 {
-	const commonItems::LocalizationDatabase database("base language", {});
+	const commonItems::LocalizationDatabase database("base_language", {});
 
 	EXPECT_EQ(database.size(), 0);
+}
+
+
+TEST(Localization_LocalizationDatabase_Tests, GettingMissingLocalizationBlockReturnsNullopt)
+{
+	const commonItems::LocalizationDatabase database("base_language", {});
+
+	EXPECT_EQ(database.GetLocalizationBlock("missing"), std::nullopt);
+}
+
+
+TEST(Localization_LocalizationDatabase_Tests, LocalizationBlockCanBeAdded)
+{
+	commonItems::LocalizationDatabase database("base_language", {"second_language"});
+
+	commonItems::LocalizationBlock block("KEY", "base_language");
+	block.ModifyLocalization("base_language", "base_value");
+	block.ModifyLocalization("second_language", "second_value");
+	database.AddOrModifyLocalizationBlock("KEY", block);
+
+	EXPECT_EQ(database.size(), 1);
+	EXPECT_TRUE(database.HasLocalization("KEY"));
+	const auto test_block = database.GetLocalizationBlock("KEY");
+	ASSERT_TRUE(test_block.has_value());
+	EXPECT_EQ(test_block->GetBaseLanguage(), "base_language");
+	EXPECT_EQ(test_block->GetKey(), "KEY");
+	EXPECT_THAT(test_block->GetLocalizations(),
+		 testing::UnorderedElementsAre(testing::Pair("base_language", "base_value"), testing::Pair("second_language", "second_value")));
+}
+
+
+TEST(Localization_LocalizationDatabase_Tests, LocalizationBlockCanBeUpdated)
+{
+	commonItems::LocalizationDatabase database("base_language", {"second_language"});
+
+	commonItems::LocalizationBlock block("KEY", "base_language");
+	block.ModifyLocalization("base_language", "base_value");
+	block.ModifyLocalization("second_language", "second_value");
+	database.AddOrModifyLocalizationBlock("KEY", block);
+
+	commonItems::LocalizationBlock second_block("KEY", "base_language");
+	second_block.ModifyLocalization("base_language", "updated_base_value");
+	second_block.ModifyLocalization("second_language", "updated_second_value");
+	database.AddOrModifyLocalizationBlock("KEY", second_block);
+
+	EXPECT_EQ(database.size(), 1);
+	EXPECT_TRUE(database.HasLocalization("KEY"));
+	const auto test_block = database.GetLocalizationBlock("KEY");
+	ASSERT_TRUE(test_block.has_value());
+	EXPECT_EQ(test_block->GetBaseLanguage(), "base_language");
+	EXPECT_EQ(test_block->GetKey(), "KEY");
+	EXPECT_THAT(test_block->GetLocalizations(),
+		 testing::UnorderedElementsAre(testing::Pair("base_language", "updated_base_value"), testing::Pair("second_language", "updated_second_value")));
 }
 
 
@@ -18,8 +71,13 @@ TEST(Localization_LocalizationDatabase_Tests, LocalizationsCanBeAddedFromStream)
 
 	std::stringstream base_input;
 	base_input << "l_base_language:\n";
-	base_input << R"( KEY:0 "value")" << "\n";
+	base_input << " KEY:0 \"value\"\n";
+	base_input << " # comment - next two lines are blank and skipped\n";
+	base_input << " \n";
+	base_input << "    \n";
+	base_input << " no_separator_skip_this_line\n";
 	base_input << R"( KEY_TWO:0 "value_two")";
+	base_input << R"( EMPTY_KEY:0)";
 	const auto base_language_lines = database.ScrapeStream(base_input);
 	EXPECT_EQ(base_language_lines, 2);
 
@@ -42,4 +100,56 @@ TEST(Localization_LocalizationDatabase_Tests, LocalizationsCanBeAddedFromStream)
 	EXPECT_EQ(test_block_two->GetBaseLanguage(), "base_language");
 	EXPECT_EQ(test_block_two->GetKey(), "KEY_TWO");
 	EXPECT_THAT(test_block_two->GetLocalizations(), testing::UnorderedElementsAre(testing::Pair("base_language", "value_two")));
+}
+
+
+TEST(Localization_LocalizationDatabase_Tests, KeylessLocalizationsAreNotAdded)
+{
+	commonItems::LocalizationDatabase database("base_language", { "second_language" });
+
+	std::stringstream base_input;
+	base_input << "l_base_language:\n";
+	base_input << R"( :0 "empty value")";
+	const auto base_language_lines = database.ScrapeStream(base_input);
+	EXPECT_EQ(base_language_lines, 0);
+
+	EXPECT_EQ(database.size(), 0);
+}
+
+
+TEST(Localization_LocalizationDatabase_Tests, ValuelessLocalizationsAreNotAdded)
+{
+	commonItems::LocalizationDatabase database("base_language", {"second_language"});
+
+	std::stringstream base_input;
+	base_input << "l_base_language:\n";
+	base_input << R"( EMPTY_KEY:0)";
+	const auto base_language_lines = database.ScrapeStream(base_input);
+	EXPECT_EQ(base_language_lines, 0);
+
+	EXPECT_EQ(database.size(), 0);
+	EXPECT_FALSE(database.HasLocalization("EMPTY_KEY"));
+}
+
+
+TEST(Localization_LocalizationDatabase_Tests, LocalizationsFromUnsupportedLanguagesWarnsAndAreNotAdded)
+{
+	commonItems::LocalizationDatabase database("base_language", {"second_language"});
+
+	std::stringstream base_input;
+	base_input << "l_third_language:\n";
+	base_input << " KEY:0 \"value\"";
+
+	std::stringstream log;
+	auto stdout_buf = std::cout.rdbuf();
+	std::cout.rdbuf(log.rdbuf());
+	const auto base_language_lines = database.ScrapeStream(base_input);
+	std::cout.rdbuf(stdout_buf);
+	std::string actual_output = log.str();
+
+	EXPECT_EQ(base_language_lines, 0);
+	EXPECT_EQ(actual_output, " [WARNING] Scraping loc line [ KEY:0 \"value\"] without language specified!\n");
+
+	EXPECT_EQ(database.size(), 0);
+	EXPECT_FALSE(database.HasLocalization("KEY"));
 }
