2fff6362b3b27f88400e45a5de025d72222b4fa0
Idhrendur <idhrendur@hotmail.com>
2022-09-15 22:52:47 -0700

    Demonstrate embeddeding lingustic rules into text file

---
+++ Linguistics/StringExtensions.cpp
@@ -0,0 +1,13 @@
+#include "StringExtensions.h"
+
+
+
+
+std::map<std::string, std::string> commonItems::GetAdjectiveRules()
+{
+	std::map<std::string, std::string> rules = {
+#include "adjectives.txt"
+	};
+
+	return rules;
+}
---
+++ Linguistics/StringExtensions.h
@@ -0,0 +1,22 @@
+#ifndef LINGUISTICS_STRING_EXTENSIONS_H
+#define LINGUISTICS_STRING_EXTENSIONS_H
+
+
+
+#include <map>
+#include <string>
+
+
+namespace commonItems
+{
+
+
+std::map<std::string, std::string> GetAdjectiveRules();
+
+
+}
+
+
+
+
+#endif // LINGUISTICS_STRING_EXTENSIONS_H
