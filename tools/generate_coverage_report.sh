56d679258ad63a3bbddd48e9ed764a167981cd18
Idhrendur <idhrendur@hotmail.com>
2022-09-25 15:19:40 -0700

    Add a script to run clang tidy (#193)
    * Configure clang-tidy
    
    * Fix some errors in linux utils
    
    * Tweak the checks used
    
    * Fix workflows

--- tools/generate_coverage_report.sh
+++ tools/generate_coverage_report.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cmake -H. -Bbuild && cmake --build ./build -- -j40
+cmake -H. -Bbuild && cmake --build --target CommonItemsTests ./build -- -j40
 cd build
 ./CommonItemsTests
 make gcov
