edebb0770478a52ca626dce88afe1fb860a737f3
Idhrendur <idhrendur@hotmail.com>
2024-12-23 20:52:54 -0800

    No need for gcov

--- build_linux.sh
+++ build_linux.sh
@@ -4,5 +4,5 @@
 export CC=/usr/bin/gcc-11 &&
 export CXX=/usr/bin/g++-11 &&
 
-cmake -H. -Bbuild &&
-cmake --build build --target CommonItemsTests -- -j40
+cmake --preset x64-release-linux
+cmake --build --preset build-x64-release-linux --target CommonItemsTests -- -j40
--- build_linux_foreign.sh
+++ build_linux_foreign.sh
@@ -4,5 +4,5 @@
 export CC=/usr/bin/gcc-11 &&
 export CXX=/usr/bin/g++-11 &&
 
-cmake -H. -Bbuild &&
-cmake --build build --target CommonItemsTests -- -j3
+cmake --preset x64-release-linux
+cmake --build --preset build-x64-release-linux --target CommonItemsTests -- -j40
